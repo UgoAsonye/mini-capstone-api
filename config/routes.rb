34148a64a8db37843c405614d21a87e400d8abf2
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-07-05 20:17:01 -0500

    Add routes to controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   get "/all_products" => "products#all_products"
   get "/Pan" => "products#Pan"
   get "/Pot" => "products#Pot"
