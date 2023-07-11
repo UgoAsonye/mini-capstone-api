0f24c4fcc07ae78ba86926915baa6f30f4e49282
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-07-10 20:35:49 -0500

    added more model methods and tests.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,16 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
-  end
-
-  def pan
-    product = Product.first
-    render json: product.as_json
-  end
-
-  def pot
-    product = Product.second
-    render json: product.as_json
-  end
-
-  def spatula
-    product = Product.third
-    render json: product.as_json
+    @products = Product.all
+    render template: "products/index"
   end
 
   def show
-    product = Product.find_by(id: params["id"])
-    render json: product.as_json
+    @product = Product.find_by(id: params["id"])
+    render template: "products/show"
   end
 
   def create
-    product = Product.create(
+    @product = Product.create(
       # name: "Cast Iron Wok",
       # price: 65,
       # image_url: "https://secure.lodgecastiron.com/on/demandware.static/-/Sites-www-lodge-com/default/dw1e15445e/images/L14W/L14W_14-Inch-Wok_Whitetable1_800x800_WEB.jpg",
@@ -35,18 +20,18 @@ class ProductsController < ApplicationController
       image_url: params["image_url"],
       description: params["description"],
     )
-    render json: product.as_json
+    render template: "products/show"
   end
 
   def update
-    product = Product.find_by(id: params["id"])
-    product.update(
-      name: params["name"] || product.name,
-      price: params["price"] || product.price,
-      image_url: params["image_url"] || product.image_url,
-      description: params["description"] || product.description,
+    @product = Product.find_by(id: params["id"])
+    @product.update(
+      name: params["name"] || @product.name,
+      price: params["price"] || @product.price,
+      image_url: params["image_url"] || @product.image_url,
+      description: params["description"] || @product.description,
     )
-    render json: product.as_json
+    render template: "products/show"
   end
 
   def destroy
