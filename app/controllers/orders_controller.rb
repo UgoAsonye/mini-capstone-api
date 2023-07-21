3995ee55d0284ebcede391469f3898bf606acbd7
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-07-20 20:08:57 -0500

    adds tests for admin and also admin functionality through migration etc.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,36 +1,51 @@
 class OrdersController < ApplicationController
   def create
-    product = Product.find_by(id: params[:product_id])
-    price = product.price
+    if current_user
+      product = Product.find_by(id: params[:product_id])
+      price = product.price
 
-    subtotal = price * params[:quantity].to_i
-    tax = 0.09 * subtotal
-    total = subtotal + tax
+      subtotal = price * params[:quantity].to_i
+      tax = 0.09 * subtotal
+      total = subtotal + tax
 
-    @order = Order.create(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: subtotal,
-      tax: tax,
-      total: total,
-    )
-    if @order.valid?
-      render :show
+      @order = Order.create(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: subtotal,
+        tax: tax,
+        total: total,
+      )
+
+      if @order.valid?
+        render :show
+      else
+        render json: { errors: @order.errors.full_messages }, status: 422
+      end
     else
-      render json: { errors: @order.errors.full_messages }, status: 422
+      render json: [], status: unauthorized
     end
   end
 
   def show
-    @order = current_user.orders.find_by(
-      id: params["id"],
-    )
-    render :show
+    if current_user
+      @order = current_user.orders.find_by(id: params[:id])
+    else
+      render json: [], status: unauthorized
+    end
+    if @order
+      render :show
+    else
+      render json: { error: "Not for you!" }
+    end
   end
 
   def index
-    @orders = current_user.orders
-    render :index
+    if current_user
+      @orders = current_user.orders
+      render :index
+    else
+      render json: [], status: unauthorized
+    end
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,6 +4,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @user = User.create(name: "Admin", email: "admin@info.com", password: "password", password_confirmation: "password", admin: true)
+
+    post "/sessions.json", params: { email: "admin@info.com", password: "password" }
+
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
   test "index" do
     get "/products.json"
     assert_response 200
@@ -20,24 +28,55 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal ["id", "name", "price", "images", "description", "is_discounted?", "tax", "total", "supplier"], data.keys
   end
   test "create" do
+    #Testing a successful response
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test product", price: 1, description: "test description", supplier_id: Supplier.first.id }
+      post "/products.json",
+           headers: { "Authorization" => "Bearer #{@jwt}" },
+           params: { name: "test product", price: 1, description: "test description", supplier_id: Supplier.first.id }
     end
+
+    #Testing product create w/o params (faild validations)
+    post "/products.json",
+         headers: { "Authorization" => "Bearer #{@jwt}" },
+         params: {}
+    assert_response 422
+
+    #Testing a product create when user is not an admin
+    post "/products.json"
+    assert_response 401
   end
 
   test "update" do
     product = Product.first
-    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+
+    #Successful product update test
+    patch "/products/#{product.id}.json",
+          headers: { "Authorization" => "Bearer #{@jwt}" },
+          params: { name: "Updated name" }
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
+
+    #Test unsuccessful product update from admin
+    patch "/products/#{product.id}.json",
+          headers: { "Authorization" => "Bearer #{@jwt}" },
+          params: { name: "" }
+    assert_response 422
+
+    #tests unsuccessful product update from non-admin
+    patch "/products/#{product.id}.json"
+    assert_response 401
   end
 
   test "destroy" do
+    #tests successful destroy from admin
     assert_difference "Product.count", -1 do
-      delete "/products/#{Product.first.id}.json"
+      delete "/products/#{Product.first.id}.json",
+             headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
+    delete "/products/#{Product.first.id}.json"
+    assert_response 401
   end
 end
